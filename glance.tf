5642ff93357cbaf1df3120552ca11dda1d77b875
Ricahrd E <rdeseke@swcp.com>
2026-05-12 18:01:52 +0200

    update glance no periods

--- glance.tf
+++ glance.tf
@@ -2,10 +2,10 @@
 ##
 # 
 
-data "glance_ubuntu_image" "ubuntu_24.04"{
+data "glance_ubuntu_image" "ubuntu_24-04"{
   source = "/v2/images/2cf93f7d-8a8f-4153-b7c7-aaaa54ae1e98/file"
 
-  image_name       = "Ubuntu-24.04"
+  image_name       = "Ubuntu-24-04"
   most_recent       = true
 }
 #  disk_format      = "qcow2"
