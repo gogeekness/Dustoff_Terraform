ad8ca0b89bbfc1ec52fbcc1ca521e1d22267d2f0
Ricahrd E <rdeseke@swcp.com>
2026-05-22 17:50:22 +0200

    Fixed a path

--- instance.tf
+++ instance.tf
@@ -1,6 +1,6 @@
 # ── Call the network module ───────────────────────────────────────────────────
 module "lustre_net" {
-  source = "~/Documents/Git/Terraform_Lustre/Lustre_Net/main.tf"
+  source = "./~/Documents/Git/Terraform_Lustre/Lustre_Net/main.tf"
   # /home/ubuntu/Terraform_Lustre/Lustre_Net
 
   external_network_name = var.external_network_name
