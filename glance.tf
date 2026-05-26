b50e8dab2782eed9c08dd3c384d9496c3b8ba1c7
Ricahrd E <rdeseke@swcp.com>
2026-05-26 10:04:59 +0200

    fixing glace data resource

--- glance.tf
+++ glance.tf
@@ -2,8 +2,8 @@
 ##
 # 
 
-data "glance_ubuntu_image" "ubuntu_24-04"{
-  source            = "/v2/images/2cf93f7d-8a8f-4153-b7c7-aaaa54ae1e98/file"
+data "openstack_images_image_v2" "ubuntu_24-04"{
+  # source            = "/v2/images/2cf93f7d-8a8f-4153-b7c7-aaaa54ae1e98/file"
   name              = "Ubuntu 24.04 LTS"
   most_recent       = true
 }
