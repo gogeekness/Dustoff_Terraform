a051971cc462cdbf6f8136bd0eb4aa8cff6856c0
goggeekness <rdeseke@swcp.com>
2026-06-12 13:45:37 +0200

    Needed to change the key name

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ module "lust_net" {
 # need to expand path 
 resource "openstack_compute_keypair_v2" "lustre_key" {
   name       = "lustre_key"
-  public_key = file(pathexpand("~/.ssh/ed_lustre.pub"))
+  public_key = file(pathexpand("~/.ssh/lustretest.pub"))
 }
 
 data "openstack_images_image_v2" "ubuntu_24-04"{
