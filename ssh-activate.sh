f1a9356b2a309512109ab6020cc4c02aaf666a70
Richard <rdeseke@gmail.com>
2026-07-01 11:39:09 +0200

    Update ssh-activate.sh

--- ssh-activate.sh
+++ ssh-activate.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+### A script to add ssh-keys for github ssh access
+### It checks for previous agents and removes them
+### Adds the ssh-keys at the end
+### run a source 'source ssh-activate.sh'
+
 agent_ok() { ssh-add -l > /dev/null 2>&1 || [ $? -eq 1 ]; }
 if ! agent_ok; then
     if [ -e ~/.ssh/agent.info ]; then
